d55f6ccd14fd96f8737222dd05a10f6bce2525ce
Øyvind Håkestad <oyvindeh@opera.com>
2012-06-20 13:52:03 +0200

    Refactor tests: Add asserts, remove fixture for swapping words.

--- test/test.js
+++ test/test.js
@@ -9,6 +9,7 @@ if (typeof require !== "undefined") {
     var lib = require("../flipcss");
 }
 
+
 buster.assertions.add("pathFlipsTo", {
     assert: function (inputPath, expectedOutputPath) {
         var input = fs.readFileSync(inputPath).toString();
@@ -20,6 +21,7 @@ buster.assertions.add("pathFlipsTo", {
     assertMessage: "Expected ${0} to flip to ${1}, got \"${output}\".",
     refuteMessage: "Expected ${0} to not flip to ${1}, got \"${output}\"."
 });
+
 
 buster.assertions.add("flipsTo", {
     assert: function (input, expectedOutput) {
@@ -63,12 +65,89 @@ buster.testCase("Functional tests: Flip stylesheet w/ pre-processing", {
     }
 });
 
-buster.testCase("Flip CSS", {
-    "swap words ('left' and 'right')": function() {
-        assert.pathFlipsTo("fixtures/input_swap_words.css",
-                           "fixtures/output_swap_words.css");
-    },
 
+buster.testCase("CSS word swapper", {
+    "swaps floats": function() {
+        // Basic case: Swap right with left
+        assert.flipsTo(".foo { float: right; }",
+                       ".foo { float: left; }");
+        // Basic case: Swap left with right
+        assert.flipsTo(".foo { clear: left; }",
+                       ".foo { clear: right; }");
+        // Extra keyword: Swap left with right
+        assert.flipsTo(".foo { float: right !important; }",
+                       ".foo { float: left !important; }");
+        // No whitespace: Swap left with right
+        assert.flipsTo(".foo{float:right !important;}",
+                       ".foo{float:left !important;}");
+        // Extra whitespace: Swap left with right
+        assert.flipsTo("  .foo  {  float:right  !important  ;  }  ",
+                       "  .foo  {  float:left  !important  ;  }  ");
+    },
+    "swaps text-align": function() {
+        // Basic case: Swap left with right
+        assert.flipsTo(".foo { text-align: left; }",
+                       ".foo { text-align: right; }");
+    },
+    "swaps margins and paddings": function() {
+        // Basic case: Swap margin-left with margin-right
+        assert.flipsTo(".foo { margin-left: 2em; }",
+                       ".foo { margin-right: 2em; }");
+        // Basic case: Swap padding-left with padding-right
+        assert.flipsTo(".foo { padding-left: 2em; }",
+                       ".foo { padding-right: 2em; }");
+        // Extra keyword: Swap padding-left with padding-right
+        assert.flipsTo(".foo { padding-left: 2em !important; }",
+                       ".foo { padding-right: 2em !important; }");
+    },
+    "swaps left and right positioning": function() {
+        // Basic case: Swap left with right
+        assert.flipsTo(".foo { left: 10px; }",
+                       ".foo { right: 10px; }");
+    },
+    "understands the difference between words and subwords": function() {
+        // "Copyright" should be unchanged (full word), but float should be changed.
+        assert.flipsTo(".copyright { float: right; }",
+                       ".copyright { float: left; }");
+        // "rights.png" Should not be changed (subword)
+        assert.flipsTo("background: url('rights.png')",
+                       "background: url('rights.png')");
+        // "arrow-left.png" Should be changed (subword)
+        assert.flipsTo("background: url('arrow-left.png')",
+                       "background: url('arrow-right.png')");
+        // "pull-right" should be changed (subword), and float should be changed
+        assert.flipsTo(".pull-right { float: right; }",
+                       ".pull-left { float: left; }");
+    },
+    "values": function() {
+        assert.pathFlipsTo("fixtures/input_swap_values.css",
+                           "fixtures/output_swap_values.css");
+    },
+    "(background position values)": function() {
+        assert.pathFlipsTo("fixtures/input_background_position.css",
+                           "fixtures/output_background_position.css");
+    },
+    "(ignored rules)": function() {
+        // Basic case: Nothing should change.
+        assert.flipsTo(".foo { clear: left; /* !direction-ignore */ }",
+                       ".foo { clear: left; /* !direction-ignore */ }");
+        // Extra keywords: Nothing should change.
+        assert.flipsTo(".foo { clear: left !important; /* !direction-ignore */ }",
+                       ".foo { clear: left !important; /* !direction-ignore */ }");
+        // Without whitespace: Nothing should change.
+        assert.flipsTo(".foo{clear:left;/*!direction-ignore*/}",
+                       ".foo{clear:left;/*!direction-ignore*/}");
+        // Extra whitespace: Nothing should change.
+        assert.flipsTo("  .foo {  clear:  left  !important;  /*  !direction-ignore  */  }  ",
+                       "  .foo {  clear:  left  !important;  /*  !direction-ignore  */  }  ");
+        // Newline before comment: Nothing should change, except newline should be removed.
+        assert.flipsTo(".foo { clear: left !important;\n /* !direction-ignore */ }",
+                       ".foo { clear: left !important; /* !direction-ignore */ }");
+    }
+});
+
+
+buster.testCase("Flip CSS", {
     "swap values (margin/padding)": function() {
         assert.pathFlipsTo("fixtures/input_swap_values.css",
                            "fixtures/output_swap_values.css");
@@ -79,6 +158,7 @@ buster.testCase("Flip CSS", {
                            "fixtures/output_background_position.css");
     }
 });
+
 
 buster.testCase("Direction specific CSS (cleaning)", {
     "add direction rule to body": function() {
